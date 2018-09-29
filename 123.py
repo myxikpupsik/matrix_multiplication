8c878f1b89eaa2edc722805d8e2b980b4e166004
unknown <myxikpupsik@gmail.com>
2018-09-29 20:54:36 +0600

    Complete algorithm

--- 123.py
+++ 123.py
@@ -1,33 +1,43 @@
-A=[]
-a=[]
-B=[]
-b=[]
+A = []
+a = []
+B = []
+b = []
 print("Enter the size of matrix 1")
-size_of_matrix_1r=int(input())
-size_of_matrix_1c=int(input())
+size_of_matrix_1r = int(input())
+size_of_matrix_1c = int(input())
 print("Enter the size of the matrix 2")
-size_of_matrix_2r=int(input())
-size_of_matrix_2c=int(input())
-if (size_of_matrix_1c!=size_of_matrix_2r):
+size_of_matrix_2r = int(input())
+size_of_matrix_2c = int(input())
+if size_of_matrix_1c != size_of_matrix_2r:
     print("multiplication cannot be done")
 print("Enter elements of the matrix from first row")
 try:
-    while (len(a)!=size_of_matrix_1c):
+    while len(a) != size_of_matrix_1c:
         for i in range(size_of_matrix_1r):
-            a=[int(x) for x in input().split()]
-            if (len(a)!=size_of_matrix_1c):
+            a = [int(x) for x in input().split()]
+            if len(a) != size_of_matrix_1c:
                 print("You entered bad data, please try again")
             A.append(a)
-except(IndexError):
+except IndexError:
     pass
-print("Enter elemetns of the matrix B")
+print("Enter elements of the matrix B")
 try:
-    while (len(b)!=size_of_matrix_2c):
+    while len(b) != size_of_matrix_2c:
         for i in range(size_of_matrix_2r):
-            b=[int(x) for x in input().split()]
-            if (len(b)!=size_of_matrix_2c):
+            b = [int(x) for x in input().split()]
+            if len(b) != size_of_matrix_2c:
                 print("You entered bad data, please try again")
             B.append(b)
-except(IndexError):
+except IndexError:
     pass
-print(A,B)
+result = [[0 for x in range(size_of_matrix_2c)] for l in range(size_of_matrix_1r)]
+for i in range(len(A)):
+    for j in range(len(B[0])):
+        for k in range(len(B)):
+            try:
+                result[i][j] += A[i][k]*B[k][j]
+            except IndexError:
+                pass
+print(result)
+
+
